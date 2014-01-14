92ef2f3801f254015f329ff38651406d02a59f8b
Magast Zheng <magast.zheng@morningstar.com>
2014-01-14 15:04:53 +0800

    update the reb-black tree

--- src/AlgorithmActionTest/Algorithm/FindLogTopKBigData.cs
+++ src/AlgorithmActionTest/Algorithm/FindLogTopKBigData.cs
@@ -118,5 +118,147 @@ namespace AlgorithmAction.Algorithm
 
             return root;
         }
+
+        /// <summary>
+        /// The 3 cases of red-black tree insertion
+        /// z stands for current node, p[z] is parent, p[p[z]] is grandparent, y is uncle
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        private RbNode InsertRebalance(RbNode node, RbNode root)
+        {
+            RbNode parent, gparent, uncle, temp;
+
+            while ((parent = node.Parent) != null && parent.Color == RbColor.Red)
+            {
+                gparent = parent.Parent;
+
+                if (parent == gparent.Left)
+                {
+                    uncle = gparent.Right;
+                    if (uncle != null && uncle.Color == RbColor.Red)
+                    {
+                        uncle.Color = RbColor.Black;
+                        parent.Color = RbColor.Black;
+                        gparent.Color = RbColor.Red;
+                        node = gparent;
+                    }
+                    else
+                    {
+                        if (parent.Right == node)
+                        {
+                            root = RotateLeft(parent, root);
+                            temp = parent;
+                            parent = node;
+                            node = temp;
+                        }
+
+                        parent.Color = RbColor.Black;
+                        gparent.Color = RbColor.Red;
+                        root = RotateRight(gparent, root);
+                    }
+                }
+                else
+                {
+                    uncle = gparent.Left;
+                    if (uncle != null && uncle.Color == RbColor.Red)
+                    {
+                        uncle.Color = RbColor.Black;
+                        parent.Color = RbColor.Black;
+                        gparent.Color = RbColor.Red;
+                        node = gparent;
+                    }
+                    else
+                    {
+                        if (parent.Left == node)
+                        {
+                            root = RotateRight(parent, root);
+                            temp = parent;
+                            parent = node;
+                            node = temp;
+                        }
+
+                        parent.Color = RbColor.Black;
+                        gparent.Color = RbColor.Red;
+                        root = RotateLeft(gparent, root);
+                    }
+                }
+            }
+
+            root.Color = RbColor.Black;
+            return root;
+        }
+
+        private RbNode SearchAuxiliary(int key, RbNode root, ref RbNode save)
+        {
+            RbNode node = root;
+            RbNode parent = null;
+            int ret;
+
+            while (node != null)
+            {
+                parent = node;
+                ret = node.Key - key;
+                if (ret > 0)
+                {
+                    node = node.Left;
+                }
+                else if (ret < 0)
+                {
+                    node = node.Right;
+                }
+                else
+                {
+                    return node;
+                }
+            }
+
+            if (parent != null)
+            {
+                save = parent;
+            }
+
+            return null;
+        }
+
+        private RbNode Search(int key, RbNode root)
+        {
+            RbNode parent = root;
+            return SearchAuxiliary(key, root, ref parent);
+        }
+
+        private RbNode Insert(int key, int data, RbNode root)
+        {
+            RbNode parent = null;
+            RbNode node = null;
+
+            node = SearchAuxiliary(key, root, ref parent);
+            if (node != null)
+            {
+                node.Data++;
+                return root;
+            }
+
+            node = CreateNode(key, data);
+            node.Parent = parent;
+            if (parent != null)
+            {
+                if (parent.Key > key)
+                {
+                    parent.Left = node;
+                }
+                else
+                {
+                    parent.Right = node;
+                }
+            }
+            else
+            {
+                root = node;
+            }
+
+            return InsertRebalance(node, root);
+        }
     }
 }
